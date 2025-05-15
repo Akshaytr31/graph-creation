ac6a8511ad5812186c2e4f029794103f78284ff6
Akshaytr31 <akshaytr31@gmail.com>
2025-05-15 20:14:25 +0530

    third

--- script.js
+++ script.js
@@ -67,8 +67,6 @@
 //   }
 // });
 
-
-
 // markBtn.addEventListener("click", () => {
 //     const x = document.getElementById("x").value;
 //     const y = document.getElementById("y").value;
@@ -117,83 +115,215 @@
 //   location.reload();
 // });
 
-
 ////////////////////////////////////////
 
+// const drawBtn = document.getElementById("drawBtn")
+// const graph = document.getElementById("graph")
+// const markBtn = document.getElementById("markBtn")
+// const clearBtn = document.getElementById("clearBtn")
+// const resetBtn = document.getElementById("resetBtn")
 
+// markBtn.disabled = true
+// markBtn.style.backgroundColor = "#cad5d7"
+// clearBtn.disabled = true
+// clearBtn.style.backgroundColor = "#cad5d7"
+// resetBtn.disabled = true
+// resetBtn.style.backgroundColor = "#cad5d7"
 
+// drawBtn.addEventListener("click", () => {
+//   const maxX = parseInt(document.getElementById("maxX").value)
+//   const maxY = parseInt(document.getElementById("maxY").value)
 
+//   graph.style.display = "grid"
+//   graph.style.gridTemplateColumns = `repeat(${maxX}, 1fr)`
+//   graph.style.gridTemplateRows = `repeat(${maxY}, 1fr)`
 
-const drawBtn = document.getElementById("drawBtn")
-const graph = document.getElementById("graph")
-const markBtn = document.getElementById("markBtn")
-const clearBtn = document.getElementById("clearBtn")
-const resetBtn = document.getElementById("resetBtn")
+//   for (let y = maxY - 1; y >= 0; y--) {
+//     for (let x = 0; x < maxX; x++) {
+//       const box = document.createElement("div")
+//       box.classList.add("grid-box")
+//       box.setAttribute("data-x", x)
+//       box.setAttribute("data-y", y)
+//       graph.appendChild(box)
+//     }
+//   }
 
-markBtn.disabled = true
-markBtn.style.backgroundColor = "#cad5d7"
-clearBtn.disabled = true
-clearBtn.style.backgroundColor = "#cad5d7"
-resetBtn.disabled = true
-resetBtn.style.backgroundColor = "#cad5d7"
+//   markBtn.disabled = false
+//   markBtn.style.backgroundColor = "rgb(50, 205, 216)"
+// })
 
-drawBtn.addEventListener("click", () => {
-  const maxX = parseInt(document.getElementById("maxX").value)
-  const maxY = parseInt(document.getElementById("maxY").value)
+// markBtn.addEventListener("click", () => {
+//   const x = parseInt(document.getElementById("x").value)
+//   const y = parseInt(document.getElementById("y").value)
+//   const maxX = parseInt(document.getElementById("maxX").value)
+//   const maxY = parseInt(document.getElementById("maxY").value)
 
-  graph.style.display = "grid"
-  graph.style.gridTemplateColumns = `repeat(${maxX}, 1fr)`
-  graph.style.gridTemplateRows = `repeat(${maxY}, 1fr)`
+//   const xInput = document.getElementById("x")
+//   const yInput = document.getElementById("y")
 
-  for (let y = maxY - 1; y >= 0; y--) {
-    for (let x = 0; x < maxX; x++) {
-      const box = document.createElement("div")
-      box.classList.add("grid-box")
-      box.setAttribute("data-x", x)
-      box.setAttribute("data-y", y)
-      graph.appendChild(box)
-    }
+//   xInput.classList.remove("outline")
+//   yInput.classList.remove("outline")
+
+//   if (x >= 0 && x < maxX && y >= 0 && y < maxY) {
+//     const target = document.querySelector(`.grid-box[data-x="${x}"][data-y="${y}"]`)
+//     if (target) {
+//       target.classList.add("marked")
+//       clearBtn.addEventListener("click", () => {
+//           target.classList.remove("marked")
+
+//       })
+//     }
+
+//     clearBtn.disabled = false
+//     clearBtn.style.backgroundColor = "rgb(50, 205, 216)"
+//     resetBtn.disabled = false
+//     resetBtn.style.backgroundColor = "rgb(50, 205, 216)"
+//   } else {
+//     if (x >= maxX || x < 0) xInput.classList.add("outline")
+//     if (y >= maxY || y < 0) yInput.classList.add("outline")
+//   }
+// })
+
+// resetBtn.addEventListener("click", () => {
+//   location.reload()
+// })
+
+////]
+
+const addGraphBtn = document.querySelector(".add-btn");
+const mainContainer = document.querySelector(".main-container");
+
+let graphCount = 0;
+
+addGraphBtn.addEventListener("click", () => {
+  const placeholder=mainContainer.querySelector('.placeholder')
+  if(placeholder){
+    placeholder.remove()
   }
+  graphCount++;
 
-
-  markBtn.disabled = false
-  markBtn.style.backgroundColor = "rgb(50, 205, 216)"
-})
-
-
-markBtn.addEventListener("click", () => {
-  const x = parseInt(document.getElementById("x").value)
-  const y = parseInt(document.getElementById("y").value)
-  const maxX = parseInt(document.getElementById("maxX").value)
-  const maxY = parseInt(document.getElementById("maxY").value)
-
-  const xInput = document.getElementById("x")
-  const yInput = document.getElementById("y")
+  const graphContainer = document.createElement("div");
+  graphContainer.innerHTML = `<div class="sub-container">
+      <h1 class="headingTwo">Graph ${graphCount}</h1>
+      <div class="input-section">
+        <div class="input-group">
+          <label >Max X</label>
+          <input type="number" id="maxX"/>
+        </div>
+        <div class="input-group">
+          <label>Max Y</label>
+          <input type="number" id="maxY"/>
+        </div>
   
-  xInput.classList.remove("outline")
-  yInput.classList.remove("outline")
+      
+        <div class="input-group">
+          <label >X</label>
+          <input type="number" id="x" class="x"/>
+        </div>
+        <div class="input-group">
+          <label>Y</label>
+          <input type="number" id="y" />
+        </div>
+      </div>
+  
+      <div class="button-section">
+        <button class="btn draw-btn" id="drawBtn">Draw</button>
+        <button class="btn mark-btn" id="markBtn">Mark</button>
+        <button class="btn clear-btn" id="clearBtn">Clear</button>
+        <button class="btn reset-btn" id="resetBtn">Reset</button>
+      </div>
+  
+      <div class="graph-container-main">
+  
+        <div id="graph" class="graph-container">
+          
+        </div>
+      </div>
+      </div>
+  `;
+  mainContainer.appendChild(graphContainer);
+  setupGraphHandlers(graphContainer);
+});
 
-  if (x >= 0 && x < maxX && y >= 0 && y < maxY) {
-    const target = document.querySelector(`.grid-box[data-x="${x}"][data-y="${y}"]`)
-    if (target) {
-      target.classList.add("marked")
+function setupGraphHandlers(elm) {
+  const drawBtn = elm.querySelector("#drawBtn");
+  const graph = elm.querySelector("#graph");
+  const markBtn = elm.querySelector("#markBtn");
+  const clearBtn = elm.querySelector("#clearBtn");
+  const resetBtn = elm.querySelector("#resetBtn");
+
+  markBtn.disabled = true;
+  markBtn.style.backgroundColor = "#cad5d7";
+  clearBtn.disabled = true;
+  clearBtn.style.backgroundColor = "#cad5d7";
+  resetBtn.disabled = true;
+  resetBtn.style.backgroundColor = "#cad5d7";
+
+  drawBtn.addEventListener("click", () => {
+    console.log("cclick");
+    const maxX = parseInt(elm.querySelector("#maxX").value);
+    const maxY = parseInt(elm.querySelector("#maxY").value);
+
+    graph.style.display = "grid";
+    graph.style.gridTemplateColumns = `repeat(${maxX}, 1fr)`;
+    graph.style.gridTemplateRows = `repeat(${maxY}, 1fr)`;
+
+    for (let y = maxY - 1; y >= 0; y--) {
+      for (let x = 0; x < maxX; x++) {
+        const box = document.createElement("div");
+        box.classList.add("grid-box");
+        box.setAttribute("data-x", x);
+        box.setAttribute("data-y", y);
+        graph.appendChild(box);
+      }
+    }
+
+    markBtn.disabled = false;
+    markBtn.style.backgroundColor = "rgb(50, 205, 216)";
+  });
+
+  markBtn.addEventListener("click", () => {
+    const x = parseInt(elm.querySelector("#x").value);
+    const y = parseInt(elm.querySelector("#y").value);
+    const maxX = parseInt(elm.querySelector("#maxX").value);
+    const maxY = parseInt(elm.querySelector("#maxY").value);
+  
+    const xInput = elm.querySelector("#x");
+    const yInput = elm.querySelector("#y");
+  
+    xInput.classList.remove("outline");
+    yInput.classList.remove("outline");
+  
+    if (x >= 0 && x < maxX && y >= 0 && y < maxY) {
+      const columnBoxes = [];
+  
+      for (let i = 0; i <= y; i++) {
+        const box = elm.querySelector(`.grid-box[data-x="${x}"][data-y="${i}"]`);
+        if (box) {
+          columnBoxes.push(box);
+        }
+      }
+  
+      columnBoxes.forEach((box) => {
+        box.classList.add("marked");
+      });
+  
       clearBtn.addEventListener("click", () => {
-          target.classList.remove("marked")
+        columnBoxes.forEach((box) => box.classList.remove("marked"));
+      });
   
-      })
+      clearBtn.disabled = false;
+      clearBtn.style.backgroundColor = "rgb(50, 205, 216)";
+      resetBtn.disabled = false;
+      resetBtn.style.backgroundColor = "rgb(50, 205, 216)";
+    } else {
+      if (x >= maxX || x < 0) xInput.classList.add("outline");
+      if (y >= maxY || y < 0) yInput.classList.add("outline");
     }
+  });
+  
 
-    clearBtn.disabled = false
-    clearBtn.style.backgroundColor = "rgb(50, 205, 216)"
-    resetBtn.disabled = false
-    resetBtn.style.backgroundColor = "rgb(50, 205, 216)"
-  } else {
-    if (x >= maxX || x < 0) xInput.classList.add("outline")
-    if (y >= maxY || y < 0) yInput.classList.add("outline")
-  }
-})
-
-resetBtn.addEventListener("click", () => {
-  location.reload()
-})
-
+  resetBtn.addEventListener("click", () => {
+    location.reload();
+  });
+}
